00623da3b24f60eb4161640b38b53b6f4b5fae1b
Jan Przedpełski <s32026@pjwstk.edu.pl>
2024-04-07 15:18:04 +0200

    PGO2

---
+++ untitled/src/com/cwiczenia/Main.java
@@ -0,0 +1,24 @@
+package com.cwiczenia;
+import models.Student;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+      Student st=new Student();
+      st.firstName="Jan";
+      st.lastName="Przedpełski";
+      st.indexNumber="s32026";
+      st.email="s32026@pjwstk.edu.pl";
+      st.address="Przy Bażantarni 13";
+      st.grades="";
+
+
+
+
+
+
+
+
+        }
+    }
---
+++ untitled/src/models/Student.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public String indexNumber;
+    public String email;
+    public String address;
+    public String grades;
+
+
+}
+
